21b994aad26ab7e0d0782d4959a5b50158464508
zaheer rahman <zaheerrahman0@gmail.com>
2024-07-01 10:33:32 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <>
+      <header className="w-full flex items-center justify-between px-[7rem] py-10">
+        <div className="text-[24px] font-bold tracking-[3px]">
+          MAX AD VISION
+        </div>
+
+        <nav>
+          <ul className="flex items-center gap-[3rem] text-[17px]">
+            <li>Home</li>
+            <li>Services</li>
+            <li>Gallery</li>
+            <li>Gallery</li>
+            <li>Project Quote</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
---
+++ src/Component/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <>
+        <div className='w-full flex items-center justify-between px-8'>
+            <div>
+                <h4>Real estate, businesses, and e-commerce.</h4>
+                <h1>Promote yourself to the fullest. Increase your vision.</h1>
+            </div>
+            <div></div>
+        </div>
+    </>
+  )
+}

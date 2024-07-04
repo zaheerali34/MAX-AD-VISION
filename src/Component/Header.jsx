f5b6a04699b5ec4fb884027779f4d79ce30149c1
zaheer rahman <zaheerrahman0@gmail.com>
2024-07-04 09:37:21 -0700

    New UpData

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMenu } from "react-icons/io5";
 
 export const Header = () => {
+
+  const [Nav, SetNav] = useState(false);
+
+  const head = ()=> {
+    SetNav(!Nav)
+  }
+
   return (
-    <header className="w-full flex items-center justify-between gap-9 px-[10rem] py-10 max-sm:px-[1rem]">
+    <header className="w-full flex items-center justify-between gap-9 px-[10rem] py-10 max-[992px]:px-[1rem]">
       <div className="text-[2rem] font-extrabold tracking-[3px] font-[Nunito] max-sm:text-[1.3rem]">MAX AD VISION</div>
 
       <nav>
@@ -17,10 +24,18 @@ export const Header = () => {
         </ul>
       </nav>
 
-    <div className="hidden max-[992px]:block text-[2rem] hover:text-[#D93020]">
+    <div onClick={head} className="hidden max-[992px]:block text-[2rem] hover:text-[#D93020] cursor-pointer">
       <span><IoMenu /></span>
     </div>
 
+    <ul className={Nav ? "fixed top-[5rem] right-0 w-full px-4 py-4 bg-white shadow-lg ease-in duration-500 flex items-center flex-col gap-6" : "w-[50%] hidden"}>
+          <li className="hover:cursor-pointer hover:text-[#D93020]">Home</li>
+          <li className="hover:cursor-pointer hover:text-[#D93020]">Services</li>
+          <li className="hover:cursor-pointer hover:text-[#D93020]">Gallery</li>
+          <li className="hover:cursor-pointer hover:text-[#D93020]">Gallery</li>
+          <li className="hover:cursor-pointer hover:text-[#D93020]">Project Quote</li>
+          <li className="hover:cursor-pointer hover:text-[#D93020]">Contact Us</li>
+        </ul>
     </header>
   );
 };
